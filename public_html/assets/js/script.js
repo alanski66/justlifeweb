659ba8b7417a3b0983bb3be0529deb7e47cdffa2
joomkit <info@joomkit.com>
2020-10-03 12:56:54 +0100

    Remove project config files

--- public_html/assets/js/script.js
+++ public_html/assets/js/script.js
@@ -17,13 +17,13 @@
     //Hero Slider
     $('.hero-slider').slick({
         autoplay: false,
-        infinite: true,
+        infinite: false,
         // speed: 3000,
         arrows: true,
         autoplaySpeed: 4000,
         prevArrow: '<button type=\'button\' class=\'prevArrow\'><i class=\'ti-arrow-left\'></i></button>',
         nextArrow: '<button type=\'button\' class=\'nextArrow\'><i class=\'ti-arrow-right\'></i></button>',
-        dots: true,
+
         customPaging: function (slider, i) {
             var icon = $(slider.$slides[i]).data('icon');
             var text = $(slider.$slides[i]).data('text');
